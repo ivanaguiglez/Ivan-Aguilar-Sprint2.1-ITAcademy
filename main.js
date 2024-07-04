9a2a0cfc50430fc1aadeffb720348222ac4b03e7
Ivan Aguilar <ivanaguiglez@gmail.com>
2024-07-04 10:58:43 +0200

    Add Bloc 1.2 Operador Ternari

--- main.js
+++ main.js
@@ -39,3 +39,43 @@ printNumber(numbersArray);
 setTimeout(() => {
   console.log("Mensaje tras 3 segundos!");
 }, "3000");
+
+// Bloc 1.2 Operador Ternari
+//1
+const potConduir = (age) => {
+  age >= 18 ? console.log("Pot conduir") : console.log("No pot conduir");
+};
+potConduir(19);
+potConduir(16);
+
+//2
+let num1 = 3;
+let num2 = 1;
+num1 >= num2
+  ? console.log("num1 és més gran")
+  : console.log("num2 és més gran");
+
+//3
+
+const trobarMaxim = (a, b, c) => (a > b ? (a > c ? a : c) : b > c ? b : c);
+
+console.log(trobarMaxim(5, 2, 3));
+
+//4
+
+const arrayNumeros = [3, 4, 5, 8];
+const numerosPares = [];
+const numerosImpares = [];
+
+const parOImpar = (arrayNumeros) => {
+  for (let i = 0; i < arrayNumeros.length; i++) {
+    let result = arrayNumeros[i] % 2 === 0 ? "Par" : "Impar";
+    console.table(result);
+  }
+};
+parOImpar(arrayNumeros);
+for (numero of arrayNumeros) {
+  numero % 2 === 0 ? numerosPares.push(numero) : numerosImpares.push(numero);
+}
+console.log("Números pares", numerosPares);
+console.log("Números impares", numerosImpares);
